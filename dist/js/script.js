a0b0ff049ef8e61c9c909731ee8aea1502ada8f3
Muhammad Awan Ardy Firmansyah <muhammadawan46@gmail.com>
2024-03-05 02:37:37 +0800

    huhh

--- dist/js/script.js
+++ dist/js/script.js
@@ -88,7 +88,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 });
 
 // mengirim email
-emailjs.sendform("service_96crtzn", "template_s6k4uyd", "#sendMessage").then(
+emailjs.sendForm("service_96crtzn", "template_s6k4uyd", "#sendMessage").then(
   (response) => {
     console.log("SUCCESS!", response.status, response.text);
     alert("Pesan terkirim!");
